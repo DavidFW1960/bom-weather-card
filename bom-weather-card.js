4a8b23643b93696bae0fb51ed4b1f3784ee69893
DavidFW1960 <36390762+DavidFW1960@users.noreply.github.com>
2022-05-21 15:36:12 +1000

    Bump version

--- bom-weather-card.js
+++ bom-weather-card.js
@@ -5,7 +5,7 @@ import {
 
 // #### Add card info to console
 console.info(
-  `%cGENERIC-WEATHER-CARD\n%cVersion 1.1.3       `,
+  `%cGENERIC-WEATHER-CARD\n%cVersion 1.1.4       `,
   "color: #043ff6; font-weight: bold; background: white",
   "color: white; font-weight: bold; background: #043ff6"
 );
